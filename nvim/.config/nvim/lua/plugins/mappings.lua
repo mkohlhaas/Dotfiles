1b6ea8fe580dd6948bee58e63f7cf6d4a0fae677
Hans-J. Schmid <knock@myopendoor.de>
2024-11-11 03:48:26 +0100

    nvim: buffer next/prev

--- nvim/.config/nvim/lua/plugins/mappings.lua
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -10,6 +10,8 @@ return {
           ["n"] = { "nzz" },
           ["N"] = { "Nzz" },
           ["G"] = { "Gzz" },
+          ["<A-j>"] = { "<cmd>bn<cr>", desc = "buffer next" },
+          ["<A-k>"] = { "<cmd>bp<cr>", desc = "buffer previous" },
         },
       },
     },
