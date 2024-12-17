b5d6016b2fd4b105a0a21d84d1b0ac57391ec5c4
Hans-J. Schmid <knock@myopendoor.de>
2024-12-17 13:44:32 +0100

    remove gleam

--- nvim/.config/nvim/lua/plugins/gleam.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "AstroNvim/astrolsp",
-  ---@param opts AstroLSPOpts
-  opts = function(_, opts)
-    opts.servers = opts.servers or {}
-    table.insert(opts.servers, "gleam")
-  end,
-}
